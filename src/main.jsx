f227eba887bbea7c901d99b28f5c10aef7f711e1
Fauricio salazar vega <fausalazar10@gmail.com>
2024-12-10 13:57:08 -0600

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {  RouterProvider } from 'react-router-dom'
-import { router } from './Router';
+import { router } from './router';
+
 
 
 
