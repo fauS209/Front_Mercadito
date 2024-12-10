bc1da1f6a7c7ae5226613c9d55e17c875fb0480f
Fauricio salazar vega <fausalazar10@gmail.com>
2024-12-10 13:48:31 -0600

    ..

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Define el directorio de salida para los archivos construidos
+    outDir: 'dist', // Define el directorio de salida para los archivos construidos
+    rollupOptions: {
+      input: 'index.html', // Asegúrate de que este sea el archivo de entrada correcto
+    },
+    sourcemap: true, // Agrega un mapa de origen para debugging (opcional)
   },
-})
+  server: {
+    open: true, // Abre el navegador automáticamente al iniciar el servidor
+  },
+});
